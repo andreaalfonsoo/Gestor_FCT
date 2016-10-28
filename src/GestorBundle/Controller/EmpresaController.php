be76060bbc455503e875600e8f690e7bae3657a6
andreaalfonsoo <andreaalfonsoo@gmail.com>
2016-10-28 10:53:19 +0200

    Subida de formulario

--- src/GestorBundle/Controller/EmpresaController.php
+++ src/GestorBundle/Controller/EmpresaController.php
@@ -3,6 +3,9 @@
 namespace GestorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use GestorBundle\Entity\Empresa;
+use GestorBundle\Form\EmpresaType;
+use Symfony\Component\HttpFoundation\Request;
 
 class EmpresaController extends Controller
 {
@@ -12,5 +15,28 @@ class EmpresaController extends Controller
         // find *all* empresas
         $empress = $repository->findAll();
         return $this->render('GestorBundle:Empresa:all.html.twig',array("empresas"=>$empress));
+    }
+
+    public function crearEmpresaAction(Request $request)
+    {
+        $empresa = new Empresa();
+        $form=$this->createForm(EmpresaType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+           // $form->getData() holds the submitted values
+           // but, the original `$task` variable has also been updated
+           $empresa = $form->getData();
+
+           // ... perform some action, such as saving the task to the database
+           // for example, if Task is a Doctrine entity, save it!
+           $em = $this->getDoctrine()->getManager();
+           $em->persist($empresa);
+           $em->flush();
+
+           return $this->redirectToRoute('all_Empresa');
+        }
+        return $this->render('GestorBundle:Empresa:new.html.twig',array("form"=>$form->createView() ));
+
     }
 }
